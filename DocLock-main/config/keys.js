04af8ce68c939395d2fd7b88f2504135a1b9d170
shreyaberi <shreyaberi5@gmail.com>
2025-10-31 03:56:25 +0530

    Initial Files

---
+++ DocLock-main/config/keys.js
@@ -0,0 +1,5 @@
+//This is mongo URI link,database is created in Atlas
+
+module.exports={
+    MongoURI:'mongodb+srv://abhishek:abhishek@cluster0.rvsxe.mongodb.net/hack36?retryWrites=true&w=majority'
+}
